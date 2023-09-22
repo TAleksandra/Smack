f655807de71a5a597254fa9f0d395c704cb2b912
TAleksandra <tatara.ola@gmail.com>
2023-09-22 02:30:10 +0200

    lesson 5

--- app/src/main/java/com/tatara/smack/CreateUserActivity.kt
+++ app/src/main/java/com/tatara/smack/CreateUserActivity.kt
@@ -1,21 +1,49 @@
 package com.tatara.smack
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import kotlin.random.Random
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    lateinit var createAvatarImageView : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
 
     fun generateUserAvatar(view: View) {
+        val random = Random
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
+
+        userAvatar = if (color == 0)  "light$avatar" else "dark$avatar"
+
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+
+        createAvatarImageView = findViewById(R.id.createAvatarImageView)
+        createAvatarImageView.setImageResource(resourceId)
 
     }
 
     fun generateColorClicked(view: View) {
+        val random = Random
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
 
+        createAvatarImageView.setBackgroundColor(Color.rgb(r, g, b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
     }
     fun createUserClicked(view: View) {
 
